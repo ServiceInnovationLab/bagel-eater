f8fde1bbd70848a0afcef31a1e3b1f908cb3731f
Merrin <merrin.macleod@gmail.com>
2019-10-23 14:42:11 +1300

    clean text

--- clean_text.rb
+++ clean_text.rb
@@ -4,14 +4,19 @@ require 'json'
 @positions = []
 
 class OnlineSubmission
-  def initialize(text)
-    @text = text
+  def initialize(text, cohort)
+    @text = text.split(/Supporting documents from your Submission/)[0]
+    @cohort = cohort
   end
 
   def clauses
     return @clauses if @clauses
     @clauses = @text.split(/Clause\n/).map do |clause|
-      clause.start_with?(/\s{0,10}\d/) ? Clause.new(clause) : nil
+      if(clause.start_with?(/\s{0,10}\d/) || clause.start_with?(/\s{0,10}Do you have any other/))
+        Clause.new(clause, @cohort)
+      else
+        nil
+      end
     end
     @clauses.compact!
   end
@@ -19,27 +24,64 @@ class OnlineSubmission
   def reference_number
     @text.scan(/Reference no: (\d{1,5})\n/).flatten.first
   end
+
+  def to_json
+    JSON.pretty_generate({
+      reference_number: reference_number,
+      clauses: clauses.map { |e| e.details }
+    })
+  end
 end
 
 class Clause
-  def initialize(text)
+  def initialize(text, cohort)
     @text = text.strip
+    @cohort = cohort
   end
 
   def question
-    return nil unless @text.include?('Position')
-    @question ||= @text.split(/Position\n/)[0]
+    @question ||= clean_whitespace(question_split[0])
+  end
+
+  def question_index
+    @cohort.question_index(question)
+  end
+
+  def answer_index
+    @cohort.answer_index(answer)
   end
 
   def answer
-    return nil unless @text.include?('Position') && @text.include?('Notes')
-    @answer ||= @text.split(/Position\n/)[1].split(/Notes\n/)[0]
+    return nil unless @text.include?('Position')
+    @answer ||= clean_whitespace(question_split[1].split(/Notes/)[0])
   end
 
   def notes
-    return nil unless @text.include?('Position') && @text.include?('Notes')
-    @notes ||= @text.split(/Position\n/)[1].split(/Notes\n/)[1]
+    @notes ||= @text.split(/Notes/)[1]&.strip!
   end
+
+  def details
+    {
+      question: question,
+      question_index: question_index,
+      answer: answer,
+      answer_index: answer_index,
+      notes: notes
+    }
+  end
+
+  private
+
+  def question_split
+    if @text.include?('Position')
+      @question_split ||= @text.split(/Position/)
+    elsif @text.include?('Notes')
+      @question_split ||= @text.split(/Notes/) #question 10 is notes only
+    else
+      @question_split ||= [nil, nil]
+    end
+  end
+
 end
 
 class SubmissionCohort
@@ -53,75 +95,63 @@ class SubmissionCohort
   end
 
   def answers
-    @answer
+    @answers
   end
 
-  def find_or_add_question
-
+  def question_index(question)
+    @questions.index(question) || new_question_index(question)
   end
 
-  def find_or_add_answer
+  def answer_index(answer)
+    @answers.index(answer) || new_answer_index(answer)
   end
-end
 
-def check_and_populate_existing_answers(q)
-  # if i = @clauses.index(q[:clause])
-  #   q[:clause] = i
-  # else
-  #   @clauses << q[:clause]
-  #   q[:clause] = @clauses.index(q[:clause])
-  # end
-  #
-  # if i = @positions.index(q[:position])
-  #   q[:position] = i
-  # else
-  #   @positions << q[:position]
-  #   q[:position] = @positions.index(q[:position])
-  # end
+  def to_json
+    JSON.pretty_generate({
+      questions: @questions,
+      answers: @answers
+    })
+  end
 
-  q
+  private
+
+  def new_question_index(question)
+    @questions += [question]
+    @questions.index(question)
+  end
+
+  def new_answer_index(answer)
+    @answers += [answer]
+    @answers.index(answer)
+  end
 end
 
 def clean_whitespace(text)
-  text.gsub!(/\s/, ' ').strip!
+  return nil unless text
+  text.gsub!(/\s{1,5}/, ' ')
+  text.gsub!(/\s{1,5}/, ' ')
+  text.strip!
 end
 
-files = Dir["./txt/*.txt"]
+def run(source_folder:, destination_folder:)
+  files = Dir["#{source_folder}/*.txt"]
+  cohort = SubmissionCohort.new
 
-files.each do |file|
-  puts "reading #{file}"
-  filename = file.split('./txt/')[1]
-  text = File.read(file)
-  answered_questions = text.scan(/^Clause\n(.*?)\nPosition\n(.*?)\nNotes\n(.*?)\n/m)
+  files.each do |file|
+    puts "reading #{file}"
+    filename = file.split("#{source_folder}/")[1]
+    text = File.read(file)
 
-  parsed_questions = []
+    submission = OnlineSubmission.new(text, cohort)
 
-  answered_questions.each do |question|
-    q = {
-      clause: clean_whitespace(question[0]),
-      position: clean_whitespace(question[1]),
-      notes: clean_whitespace(question[2])
-    }
-    q = check_and_populate_existing_answers(q)
-    parsed_questions.push(q)
+    File.open("#{destination_folder}/#{filename}.json", 'w') do |txt_file|
+      txt_file.puts(submission.to_json)
+    end
   end
 
-
-  File.open("txt_cleaned/#{filename}", 'w') do |txt_file|
-    txt_file.puts(parsed_questions.to_json)
-
-    # text.match(/Position([\w\d\s]*)Notes/)
-    #
-    # text.gsub!(/Notes\n\n\n\n/, '')
-    # text.gsub!('Your submission to Zero Carbon Bill', '')
-    # text.gsub!(/\nClause\n/, '')
-    # txt_file.puts(text)
-    puts "cleaned #{file}"
+  File.open("#{destination_folder}/meta/key.json", 'w') do |txt_file|
+    txt_file.puts(cohort.to_json)
   end
 end
 
-
-File.open("txt_cleaned/key.txt", 'w') do |txt_file|
-  txt_file.puts({clauses: @clauses}.to_json)
-  txt_file.puts({positions: @positions}.to_json)
-end
+run(source_folder: "./sorted/online", destination_folder: "./txt_cleaned")
